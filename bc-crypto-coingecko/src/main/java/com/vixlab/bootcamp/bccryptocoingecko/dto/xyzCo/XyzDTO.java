45ed4dd39edfe387c274b58303e3fb48652fbd12
charlesbombor <charlesbombor@gmail.com>
2024-02-21 12:14:43 +0800

    update

---
+++ bc-crypto-coingecko/src/main/java/com/vixlab/bootcamp/bccryptocoingecko/dto/xyzCo/XyzDTO.java
@@ -0,0 +1,16 @@
+package com.vixlab.bootcamp.bccryptocoingecko.dto.xyzCo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class XyzDTO {
+
+  private String id;
+  private String symbol;
+  private String name;
+  private String image;
+
+  
+}
